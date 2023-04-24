79da43893708db753424835440387eda7c9caef2
chanelfcheng <chanelchengc@gmail.com>
2023-04-23 23:52:29 -0400

    Can shoot bars now yayyyyyyyyyyyyy

--- gui.py
+++ gui.py
@@ -10,21 +10,14 @@ pygame.init()
 
 WINDOW_WIDTH, WINDOW_HEIGHT = pygame.display.get_desktop_sizes()[0]
 
-BAR_COLOR = pygame.Color(0, 128, 255)  # blue
-BAR_WIDTH = WINDOW_WIDTH * 0.9 * 0.05
-BAR_HEIGHT = WINDOW_HEIGHT * 0.9 * 0.89
-BAR_X = WINDOW_WIDTH * 0.9 * 0.05
-BAR_Y = WINDOW_HEIGHT * 0.9 * 0.05
+RADIUS = 70
+OFFSET = 35
 
-NOTE_COLOR = pygame.Color(0, 255, 162)  # green
-NOTE_WIDTH = 20
-NOTE_HEIGHT = 20
-NOTE_START_X = WINDOW_WIDTH + 1
+RED_HI_COLOR = pygame.Color(255, 0, 162)  # green
+RED_LO_COLOR = pygame.Color(255//2, 0, 162//2)  # green
 
-CURSOR_COLOR = pygame.Color(255, 255, 255)  # white
-CURSOR_WIDTH = BAR_WIDTH + 30
-CURSOR_HEIGHT = 150
-CURSOR_START_Y = BAR_Y + BAR_HEIGHT - CURSOR_HEIGHT
+GREEN_HI_COLOR = pygame.Color(0, 255, 0)  # green
+GREEN_LO_COLOR = pygame.Color(0, 255//2, 0)  # green
 
 #### END OF CONFIG #####
 
@@ -63,26 +56,29 @@ CURSOR_START_Y = BAR_Y + BAR_HEIGHT - CURSOR_HEIGHT
 
 class Note:
 
-    def __init__(self, pycircle: pygame.draw.circle, step_size=310) -> None:
-        self.pycircle = pycircle
+    def __init__(self, bar: pygame.Rect, color) -> None:
+        self.bar = bar
+        self.color = color
         self.cursor_touch = False
         self.frame_counter = 1
-        self.step_size = step_size
 
-    def shrink(self):
-        self.pycircle.height -= self.step_size
+    def move(self, step_size=1):
+        self.bar = self.bar.move(step_size, 0)
         
+    def get_color(self):
+        return self.color
+    
     def get_radius(self):
-        return self.pycircle.height//2
+        return self.bar.height//2
 
-    def get_pycircle(self) -> pygame.Rect:
-        return self.pycircle
+    def get_bar(self) -> pygame.Rect:
+        return self.bar
 
     def get_x(self) -> int:
-        return self.pycircle.left
+        return self.bar.left
 
     def get_y(self) -> int:
-        return self.pycircle.top
+        return self.bar.top
 
     # unused anymore
     # def is_touching_center(self, cursor_x, cursor_y):
@@ -93,119 +89,184 @@ class Note:
 
 
 class RhythmGame:
-
+    
+    # Display
+    window_width = 0.9 * WINDOW_WIDTH
+    window_height = 0.9 * WINDOW_HEIGHT
+    screen = pygame.display.set_mode(
+            (window_width, window_height), pygame.SCALED)
+    
+    # Notes
     unit_time = 250
     notes_distance = 1000
-    min_notes = 10
-    max_notes = 20
+    num_notes = [4, 8, 12]
+    max_notes = 16
+
+    green_circle_x = screen.get_width()//4
+    red_circle_x = 3*screen.get_width()//4
+    circle_y = screen.get_height()//2
 
     def __init__(self) -> None:
-        self.window_width = 0.9 * WINDOW_WIDTH
-        self.window_height = 0.9 * WINDOW_HEIGHT
-
-        self.screen = pygame.display.set_mode(
-            (self.window_width, self.window_height), pygame.RESIZABLE)
-
         # self.cursor = Cursor()
-        self.notes = []
+        self.red_notes = []
+        self.green_notes = []
+        self.hi_flag = False
+        self.start_time = time.time()
+        self.user_turn = False
+        self.num_notes = RhythmGame.num_notes[0]
+        self.red_note_counter = 0
+        self.green_note_counter = 0
+        self.is_pressing_up = False
+        self.is_pressing_down = False
 
-    def add_note(self):
+        # TODO: draw menu screen
 
-        pycircle = pygame.draw.circle(self.screen, NOTE_COLOR,
-                                    (self.screen.get_width()//2, self.screen.get_height()//2), self.screen.get_height()//2, 5)
+    def add_red_hi_note(self):
+
+        bar = pygame.draw.rect(self.screen, RED_HI_COLOR, 
+                               pygame.Rect(RhythmGame.red_circle_x - 2*RADIUS, 
+                                RhythmGame.circle_y - OFFSET, 70, 10))
         
-        note = Note(pycircle)
-        self.notes.append(note)
+        note = Note(bar, RED_HI_COLOR)
+        self.red_notes.append(note)
+
+    def add_red_lo_note(self):
+        
+        bar = pygame.draw.rect(self.screen, RED_LO_COLOR, 
+                               pygame.Rect(RhythmGame.red_circle_x - 2*RADIUS, 
+                                RhythmGame.circle_y + OFFSET, 70, 10))
+        
+        note = Note(bar, RED_LO_COLOR)
+        self.red_notes.append(note)
+
+    def add_green_hi_note(self):
+
+        bar = pygame.draw.rect(self.screen, GREEN_HI_COLOR, 
+                               pygame.Rect(RhythmGame.green_circle_x + RADIUS, 
+                                RhythmGame.circle_y - OFFSET, 70, 10))
+        
+        note = Note(bar, GREEN_HI_COLOR)
+        self.green_notes.append(note)
+
+    def add_green_lo_note(self):
+        
+        bar = pygame.draw.rect(self.screen, GREEN_LO_COLOR, 
+                               pygame.Rect(RhythmGame.green_circle_x + RADIUS, 
+                                RhythmGame.circle_y + OFFSET, 70, 10))
+        
+        note = Note(bar, GREEN_LO_COLOR)
+        self.green_notes.append(note)
 
     def update_screen(self) -> None:
         self.screen.fill((0, 0, 0))  # black
-        # draw cursor
-        self.draw_cursor()
-        # draw cursor
-        # self.cursor.move()
-        # self.draw_cursor(self.cursor.get_obj())
-        # draw all notes to the current frame
-        # self.handle_notes()
-        for note in self.notes:
-            # get the note's obj(rect)
-            self.draw_note(note.get_pycircle())
+        self.draw_circles()
 
-        # # handle notes
-        # for note in self.notes:
-        #     note.move()
-        #     if note.is_touching_cursor(self.cursor.get_x(), self.cursor.get_y()) and not note.cursor_touch:  # hit
-        #         self.add_hit_effect(note)
-        #         note.cursor_touch = True
-        #     if note.get_x() < 0:  # miss
-        #         self.notes.remove(note)
-        #         note.cursor_touch = False
-        # if (len(self.notes) == 0):
-        #     self.add_note()
+        if (time.time() - self.start_time > 1):
+            for note in self.red_notes:
+                note.move(-140)
+            
+            for note in self.green_notes:
+                note.move(140)
 
-        # render hit effects
-        i = 0
-        while i < len(self.notes):
-            note = self.notes[i]
-            radius = note.get_pycircle().height//2
-            
-            # make the hit effect discrete
-            note.frame_counter += 1
-            if (note.frame_counter % 360 == 0):
-                note.shrink()
-                note.frame_counter = 1
-            
-            
-            if radius > 30:
-                thickness = 2 - 1//(radius)
-                if thickness < 0:
-                    thickness = 0
-                # effect = pygame.draw.circle(self.screen, NOTE_COLOR,
-                #                             (effect.centerx, effect.centery),
-                #                             radius, thickness)
-                self.draw_note(note.get_pycircle())
+            self.start_time = time.time()
+            if not self.user_turn:
+                self.add_red_hi_note() if self.hi_flag else self.add_red_lo_note()
+                self.hi_flag = not self.hi_flag
+                self.red_note_counter += 1
+                if self.red_note_counter == self.num_notes:
+                    self.user_turn = True
+                    self.red_note_counter = 0
+
+            if self.user_turn:
+                key = pygame.key.get_pressed()
+
+                # up_pressed = key[pygame.K_UP]
+                if key[pygame.K_UP] and not self.is_pressing_up:
+                    # self.is_pressing_up = True
+                    self.add_green_hi_note()
+                    self.green_note_counter += 1
+                    print("generating green hi note")
+                # self.key_pressed = up_pressed
+
+                down_pressed = key[pygame.K_DOWN]
+                if key[pygame.K_DOWN] and not self.is_pressing_down:
+                    # self.is_pressing_down = True
+                    self.add_green_lo_note()
+                    self.green_note_counter += 1
+                    print("generating green lo note")
+                    
+                # self.key_pressed = down_pressed
+                # if not up_pressed and self.is_pressing_up:
+                    # self.is_pressing_up = False
+                # if not down_pressed and self.is_pressing_down:
+                    # self.is_pressing_down = False
                 
-                self.notes[i] = note
-            else:
-                self.notes.remove(note)
-                i -= 1
+                if self.green_note_counter == self.num_notes:
+                    self.user_turn = False
+                    self.green_note_counter = 0
 
-            # if effect.top <= 1 or effect.left <= 1:
-            #     self.notes.remove(effect)
-            #     i -= 1
-            i += 1
+        # draw/remove notes  
+        for note in self.red_notes:
+            # get the note's obj(rect)
+            self.draw_note(note.get_bar(), note.get_color())
+            if note.get_x() < self.green_circle_x + RADIUS:
+                self.red_notes.remove(note)
+ 
+        for note in self.green_notes:
+            # get the note's obj(rect)
+            self.draw_note(note.get_bar(), note.get_color())
+            if note.get_x() > self.red_circle_x - RADIUS - note.get_bar().width:
+                self.green_notes.remove(note)       
+
+            
 
     def display(self) -> None:
         pygame.display.flip()
 
-    def draw_cursor(self) -> None:
+    def draw_circles(self) -> None:
+        # GREEN CIRCLE
         note_color = pygame.Color(0, 255-100, 162-100)
         pygame.draw.circle(self.screen, note_color,
-                                (self.screen.get_width()//2, self.screen.get_height()//2), 70, 0)
+                                (self.screen.get_width()//4, self.screen.get_height()//2), RADIUS, 0)
         
         note_color = pygame.Color(0, 255-50, 162-50)
         pygame.draw.circle(self.screen, note_color,
-                                (self.screen.get_width()//2, self.screen.get_height()//2), 60, 0)
+                                (self.screen.get_width()//4, self.screen.get_height()//2), 60, 0)
         
         note_color = pygame.Color(0, 255, 162)
         pygame.draw.circle(self.screen, note_color,
-                                (self.screen.get_width()//2, self.screen.get_height()//2), 50, 0)
+                                (self.screen.get_width()//4, self.screen.get_height()//2), 50, 0)
 
-    def draw_note(self, pycircle) -> None:
-        pygame.draw.circle(self.screen, NOTE_COLOR,
-                                    (self.screen.get_width()//2, self.screen.get_height()//2), pycircle.height//2, 5)
+        # RED CIRCLE
+        note_color = pygame.Color(255-100, 0, 100-100)
+        pygame.draw.circle(self.screen, note_color,
+                                (3*self.screen.get_width()//4, self.screen.get_height()//2), RADIUS, 0)
+        
+        note_color = pygame.Color(255-50, 0, 100-50)
+        pygame.draw.circle(self.screen, note_color,
+                                (3*self.screen.get_width()//4, self.screen.get_height()//2), 60, 0)
+        
+        note_color = pygame.Color(255, 0, 100)
+        pygame.draw.circle(self.screen, note_color,
+                                (3*self.screen.get_width()//4, self.screen.get_height()//2), 50, 0)
+
+    def draw_note(self, bar, color) -> None:
+        # pygame.draw.circle(self.screen, NOTE_COLOR,
+        #                             (self.screen.get_width()//2, self.screen.get_height()//2), pycircle.height//2, 5)
+        pygame.draw.rect(self.screen, color, bar)
 
     # def handle_notes(self):
-    #     if len(self.notes) == 0:
+    #     if len(self.red_notes) == 0:
     #         prev_note_x = NOTE_START_X
     #         prev_note_y = random.randint(
     #             math.ceil(BAR_Y + BAR_HEIGHT/2), math.ceil(BAR_Y + BAR_HEIGHT - NOTE_HEIGHT))
 
-    #     if len(self.notes) < RhythmGame.min_notes:
+    #     if len(self.red_notes) < RhythmGame.min_notes:
     #         note_y = random.randint(math.ceil(BAR_Y), math.ceil(BAR_Y +
     #                                                             BAR_HEIGHT - NOTE_HEIGHT))
-    #         if len(self.notes) > 0:
-    #             prev_note_x = self.notes[-1].get_x()
-    #             prev_note_y = self.notes[-1].get_y()
+    #         if len(self.red_notes) > 0:
+    #             prev_note_x = self.red_notes[-1].get_x()
+    #             prev_note_y = self.red_notes[-1].get_y()
     #             RhythmGame.notes_distance = RhythmGame.unit_time * \
     #                 random.randint(1, 4)
     #             note_x = prev_note_x + RhythmGame.notes_distance
@@ -234,8 +295,13 @@ class RhythmGame:
 def main():
     game = RhythmGame()
 
+    game.display()
+    pygame.event.get()
+
     # create a random number generator
     random.seed(time.time())
+
+    time.sleep(1)
 
     # recorder, trigger, receiver = testRecorded.init_stream(
     #     bufsize=0.1, winsize=0.1)
