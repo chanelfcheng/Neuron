d475b1be44d280acbd3a754fb84c7e7067034ba3
chanelfcheng <chanelchengc@gmail.com>
2023-04-25 16:34:17 -0400

    Working on separating network layers

--- preprocessing.py
+++ preprocessing.py
@@ -2,7 +2,7 @@ import mne
 import numpy as np
 import pandas as pd
 
-def load_data(filename):
+def load_file_data(filename):
     df = pd.read_csv(filename, delimiter=",")
     df = df.drop(df.index[0])   # Drop the first row
     df = df.drop(columns=['Sample Index'])   # Drop the first col
@@ -19,6 +19,14 @@ def load_data(filename):
 
     return raw
 
+def load_np_data(np_array):
+    np_array = np_array[:8, :]
+    print(np_array.shape)
+    ch_names = ["Fp1", "Fp2", "C3", "C4", "P3", "P4", "O1", "O2"]
+    raw = mne.io.RawArray(np_array, mne.create_info(ch_names, 250, ch_types="eeg"))
+
+    return raw
+
 def filter_eeg_data(raw):
     # Apply notch filter to remove 60Hz noise
     filtered = raw.copy().notch_filter(60, filter_length="auto", phase="zero")
@@ -28,9 +36,25 @@ def filter_eeg_data(raw):
 
     return filtered
 
-def remove_artifacts(filtered):
+def compute_psd(filtered):
+    # Compute PSD
+    psd = filtered.compute_psd(method='multitaper', fmin=1, fmax=50)
+
+    return psd
+
+def compute_bands(psd):
+    # Get delta, theta, alpha, beta, and gamma bands
+    delta = psd.get_data(picks=[0, 1, 2, 3, 4, 5, 6, 7], fmin=1, fmax=4).mean()
+    theta = psd.get_data(picks=[0, 1, 2, 3, 4, 5, 6, 7], fmin=4, fmax=8).mean()
+    alpha = psd.get_data(picks=[0, 1, 2, 3, 4, 5, 6, 7], fmin=8, fmax=13).mean()
+    beta = psd.get_data(picks=[0, 1, 2, 3, 4, 5, 6, 7], fmin=13, fmax=30).mean()
+    gamma = psd.get_data(picks=[0, 1, 2, 3, 4, 5, 6, 7], fmin=30, fmax=50).mean()
+
+    return delta, theta, alpha, beta, gamma
+
+def remove_artifacts(filtered, pov=0.9):
     # Apply ICA to remove artifacts
-    ica = mne.preprocessing.ICA(n_components=0.95, random_state=97)
+    ica = mne.preprocessing.ICA(n_components=pov)#, random_state=97)
     ica.fit(filtered);
     ica.exclude = [];
     # ica.plot_sources(filtered)
@@ -42,4 +66,4 @@ def remove_artifacts(filtered):
     return filtered
 
 if __name__ == "__main__":
-    raw_focus = load_data("OpenBCISession_steve_focus1/OpenBCI-RAW-2023-04-13_17-26-20.csv")
+    raw_focus = load_file_data("OpenBCISession_steve_focus1/OpenBCI-RAW-2023-04-13_17-26-20.csv")
--- record.py
+++ record.py
@@ -1,15 +1,123 @@
 import argparse
+import time
+import numpy as np
+import pandas as pd
+import torch
+from brainflow.board_shim import BoardShim, BrainFlowInputParams, BoardIds, BrainFlowPresets
+from preprocessing import load_np_data, filter_eeg_data, compute_psd, compute_bands
+from snn import SNN
+
+STREAM_NAME = 'OpenBCI_EEG'
+
+def init_stream():
+    params = BrainFlowInputParams()
+    params.serial_port = "/dev/cu.usbserial-DM00CVFL"
+
+    board = BoardShim(BoardIds.CYTON_BOARD, params)
+    board.prepare_session()
+    board.start_stream()
+
+    return board
+
+def read_labeled_data(board, label):
+    time.sleep(0.5)
+    # data = board.get_current_board_data (256) # get latest 256 packages or less, doesnt remove them from internal buffer
+    data = board.get_board_data()  # get all data and remove it from internal buffer
+
+    raw = load_np_data(data)
+    filtered_data = filter_eeg_data(raw)
+    psd = compute_psd(filtered_data)
+    delta, theta, alpha, beta, gamma = compute_bands(psd)
+
+    # make numpy array with the bands and labels
+    data = np.array([delta, theta, alpha, beta, gamma, label])
+
+    return data
+
+def read_unlabeled_data(board):
+    time.sleep(0.5)
+    # data = board.get_current_board_data (256) # get latest 256 packages or less, doesnt remove them from internal buffer
+    data = board.get_board_data()  # get all data and remove it from internal buffer
+
+    raw = load_np_data(data)
+    filtered_data = filter_eeg_data(raw)
+    psd = compute_psd(filtered_data)
+    delta, theta, alpha, beta, gamma = compute_bands(psd)
+    
+    data = np.array([delta, theta, alpha, beta, gamma])
+
+    return data
+
+def stop_stream(board):
+    board.stop_stream()
+    board.release_session()
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--calibrate", help="whether to calibrate or not", action="store_true")
-
     args = parser.parse_args()
 
+    board = init_stream()
+
     if args.calibrate:
-        print("Calibrating Input...")
+        print("Calibrating input...")
+
+        # Initialize model with device
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        model = SNN(device)
+
+        # Load in pre-trained model parameters
+        model.load_state_dict(torch.load('snn.pt'))
+
+        # Set require grad to false
+        for param in model.parameters():
+            param.requires_grad = False
+
+        # Initialize new final layer of model
+        model.fc = torch.nn.Linear(5, 1)
+
+        # Initialize calibration dataset
+        dataset = np.array([])
+
+        for i in range(3):
+            input("Press enter to calibrate high focus...")
+            data = read_labeled_data(board, label=1)
+            print(data)
+
+            if dataset.size == 0:
+                dataset = data
+            else:
+                dataset = np.vstack((dataset, data))
+
+        print("Done calibrating high focus!\n")
+        
+        for i in range(3):
+            input("Press enter to calibrate low focus...")
+            data = read_labeled_data(board, label=0)
+            print(data)
+            
+            dataset = np.vstack((dataset, data))
+        
+        print("Done calibrating low focus!\n")
+
+        print("Run the script again to re-calibrate.")
+
+        
     else:
-        print("Reading Input...")
+        print("Reading input...")
+
+        while True:
+            print("Press Ctrl+C to stop.")
+            # check keyboard interrupt
+            try:
+                # read data
+                data = read_unlabeled_data(board)
+                print(data)
+            except KeyboardInterrupt:
+                break
+
+    stop_stream(board)
+    print("Stream closed.")
 
 if __name__ == "__main__":
     main()
